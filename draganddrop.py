e1426192f40f9f24f3633589e93cc8cb48db8a55
umer3 <40017513+umer3@users.noreply.github.com>
2019-12-24 15:21:42 +0500

    Update draganddrop.py

--- draganddrop.py
+++ draganddrop.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import re
+
 import unittest
 from selenium.webdriver import ActionChains
 
